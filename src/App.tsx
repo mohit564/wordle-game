43f0b0e95796cf65ddeebe0bda44a54e9ab82e94
mohit564 <86644125+mohit564@users.noreply.github.com>
2023-01-21 00:16:35 +0530

    completed 🚀

--- src/App.tsx
+++ src/App.tsx
@@ -11,22 +11,28 @@ import { getRandomWord } from "./utils/word";
 const App = () => {
   const inputRef = useRef<OtpInput | null>(null);
 
-  const [word, setWord] = useState<string>("");
+  const [attempt, setAttempt] = useState<number>(0);
+  const [responses, setResponses] = useState<string[]>(Array(6).fill(""));
   const [target, setTarget] = useState<string>(getRandomWord());
+  const [containerClasses, setContainerClasses] = useState<string[][]>(
+    Array(6).fill([""])
+  );
+  const [resultMessage, setResultMessage] = useState<string>("");
   const [isInputDisabled, setInputDisabled] = useState<boolean>(false);
-  const [containerClasses, setContainerClasses] = useState<string[]>([]);
 
-  const handleInputChange = (text: string) => {
+  const handleInputChange = (text: string, index: number) => {
     if (!text.match(/^[a-zA-Z]+$/)) {
-      if (inputRef.current) inputRef.current.focusInput(word.length);
+      inputRef.current?.focusInput(responses[index].length);
       return;
     }
 
-    setWord(text.toUpperCase());
+    let temp = [...responses];
+    temp[index] = text.toUpperCase();
+    setResponses(temp);
   };
 
   const handleSubmit = () => {
-    if (word.length < 5) {
+    if (responses[attempt].length < 5) {
       toast("Please enter all letters", {
         duration: 1000,
         icon: "🙏",
@@ -36,24 +42,33 @@ const App = () => {
 
     let classes = ["submitted"];
 
-    for (let i = 0; i < word.length; i++) {
-      if (word.charAt(i) === target.charAt(i)) {
+    for (let i = 0; i < 5; i++) {
+      if (responses[attempt].charAt(i) === target.charAt(i)) {
         classes.push(`correct${i + 1}`);
         continue;
       }
-      if (target.includes(word.charAt(i))) {
+      if (target.includes(responses[attempt].charAt(i))) {
         classes.push(`present${i + 1}`);
       }
     }
 
-    setContainerClasses(classes);
-    setInputDisabled(true);
+    let temp = [...containerClasses];
+    temp[attempt] = classes;
+    setContainerClasses(temp);
+
+    if (responses[attempt] === target) {
+      setResultMessage("You Won. Congratulations!");
+    }
+
+    setAttempt((prev) => prev + 1);
   };
 
   const handleReset = () => {
-    setWord("");
+    setAttempt(0);
+    setResponses(Array(6).fill(""));
     setTarget(getRandomWord());
-    setContainerClasses([]);
+    setContainerClasses(Array(6).fill([]));
+    setResultMessage("");
     setInputDisabled(false);
   };
 
@@ -61,31 +76,39 @@ const App = () => {
     console.log(`Answer: ${target}`);
   }, [target]);
 
-  const resultMessage =
-    word === target ? "You Won. Congratulations!" : "You Lost. Try Again!";
+  useEffect(() => {
+    if (attempt > 5) {
+      setResultMessage(`Answer was ${target}. Best Luck Next Time!`);
+      setInputDisabled(true);
+    }
+    inputRef.current?.focusInput(0);
+  }, [attempt]);
 
   return (
     <>
       <h2 className="heading">Wordle Game</h2>
-      {isInputDisabled && <h3 className="result">Result : {resultMessage}</h3>}
-      <OtpInput
-        ref={inputRef}
-        value={word}
-        onChange={handleInputChange}
-        numInputs={5}
-        containerStyle={`container ${containerClasses.join(" ")}`}
-        inputStyle={"input-boxes"}
-        focusStyle={!isInputDisabled ? "input-focus" : ""}
-        onSubmit={handleSubmit}
-        shouldAutoFocus={true}
-        isDisabled={isInputDisabled}
-      ></OtpInput>
-      {(!isInputDisabled || word.length < 5) && (
+      {resultMessage && <h3 className="result">{resultMessage}</h3>}
+      {responses.map((_, index) => (
+        <OtpInput
+          key={index}
+          ref={index === attempt ? inputRef : null}
+          value={responses[index]}
+          onChange={(text) => handleInputChange(text, index)}
+          numInputs={5}
+          containerStyle={`container ${containerClasses[index].join(" ")}`}
+          inputStyle={"input-boxes"}
+          focusStyle={index === attempt && !resultMessage ? "input-focus" : ""}
+          onSubmit={handleSubmit}
+          shouldAutoFocus={true}
+          isDisabled={isInputDisabled || Boolean(resultMessage)}
+        />
+      ))}
+      {attempt <= 5 && !resultMessage && (
         <button className="submit" onClick={handleSubmit}>
           Submit
         </button>
       )}
-      {isInputDisabled && word.length === 5 && (
+      {resultMessage && (
         <button className="reset" onClick={handleReset}>
           Reset
         </button>
